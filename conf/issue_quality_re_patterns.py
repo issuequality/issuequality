c604802eda1d5edeb0f8590cade6a677c3dac8cf
Vagner Clementino <vagner.clementino@gmail.com>
2017-03-15 06:26:38 -0300

    Incluindo padrão de re para anexos

--- conf/issue_quality_re_patterns.py
+++ conf/issue_quality_re_patterns.py
@@ -3,5 +3,8 @@
 
 # Padrões de RE para Github Formatted Markdown
 gfm_pat = {
-    'list': r'[*,-]\s.+|\d+\.\s+.+'
+    'list': r'[*,-]\s.+|\d+\.\s+.+',
+    'attach': (r'(\[.+\.PNG\]|\[.+\.GIF\]|\[.+\.JPG\]|\[.+\.DOCX\]|')
+              ('\[.+\.PPTX\]|\[.+\.XLSX]|\[.+\.TXT\]|\[.+\.PDF\]|')
+              ('\[.+\.ZIP\])(\(https\:\/\/github\.com\/.+\))')
 }
